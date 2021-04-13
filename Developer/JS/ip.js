1632eb7594cc6e70dfb2302e29438c49d402196b
ex3-aimatrix <78482126+ex3-aimatrix@users.noreply.github.com>
2021-04-13 10:54:11 +0400

    New Update

--- Developer/JS/ip.js
+++ Developer/JS/ip.js
@@ -1,7 +1,7 @@
 var ip;
  $.getJSON("https://api.ipify.org?format=json", function(data) {  
 	ip = data.ip;       
-	$.post('http://ex3-aimatrix.herokuapp.com/glsry/ip', {ip:ip}).done(function(response){
+	$.post('https://ex3-aimatrix.herokuapp.com/glsry/ip', {ip:ip}).done(function(response){
       
 	});
  })
